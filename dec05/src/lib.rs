511f54e29873d2ce12b2348e0bc26f7183e76b88
martin sereinig <ms@veryunited.net>
2024-04-19 23:40:04 +0200

    working for test input

--- dec05/src/lib.rs
+++ dec05/src/lib.rs
@@ -1,12 +1,22 @@
 use std::fmt;
 
-struct Command {
-    source: i32,
-    destination: i32,
-    count: i32,
+pub struct Command {
+    pub source: i32,
+    pub destination: i32,
+    pub count: i32,
 }
 
-fn parse_command(command: &str) -> Command {
+impl fmt::Display for Command {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "move {} from {} to {}",
+            self.count, self.source, self.destination
+        )
+    }
+}
+
+pub fn parse_command(command: &str) -> Command {
     let parts: Vec<&str> = command.split("from").collect(); // move 1 from 2 to 1
     let move_part = parts[0]; // move 1
     let location_part = parts[1]; // 2 to 1
@@ -25,7 +35,7 @@ fn parse_command(command: &str) -> Command {
     }
 }
 
-struct Stack {
+pub struct Stack {
     number: i32,
     crates: Vec<Crate>,
 }
@@ -54,9 +64,51 @@ impl Stack {
     pub fn push(&mut self, push: &mut Vec<Crate>) {
         self.crates.append(push);
     }
+
+    pub fn top_crate(&self) -> char {
+        self.crates.last().unwrap().symbol
+    }
 }
 
-struct Crate {
+pub fn parse_stacks(lines: Vec<&str>) -> Vec<Stack> {
+    // // stacks
+    // let lines_before_empty_line: Vec<&str> = parts[0].lines().collect();
+    let mut stacks: Vec<Stack> = Vec::new();
+
+    // first, get the index of all the stacks and create the empty stacks
+    let baseline = lines.last().unwrap();
+    let mut positions: Vec<usize> = Vec::new();
+    for (index, ch) in baseline.char_indices() {
+        if ch.is_digit(10) {
+            positions.push(index);
+            stacks.push(Stack {
+                number: ch.to_string().parse::<i32>().unwrap(),
+                crates: vec![],
+            });
+        }
+    }
+    let positions = positions; // we won't mutate it anymore
+
+    // now fill the stacks backwards
+    for line in lines.iter().rev().skip(1) {
+        println!("{}", line);
+        for (stack_index, &crate_index) in positions.iter().enumerate() {
+            let crate_symbol = line.chars().nth(crate_index);
+            match crate_symbol {
+                Some(symbol) => {
+                    if !symbol.is_whitespace() {
+                        let mut new_crate = vec![Crate { symbol }];
+                        stacks[stack_index].push(&mut new_crate);
+                    }
+                }
+                None => continue,
+            }
+        }
+    }
+    stacks
+}
+
+pub struct Crate {
     symbol: char,
 }
 
@@ -116,6 +168,19 @@ mod tests {
     }
 
     #[test]
+    fn can_parse_stacks() {
+        let stack_description = r#"    [D]
+    [N] [C]
+[Z] [M] [P]
+ 1   2   3"#;
+        println!("geilo!!");
+        let stacks = parse_stacks(stack_description.lines().collect());
+        assert_eq!("1: [Z]", &stacks[0].to_string());
+        assert_eq!("2: [M] [N] [D]", &stacks[1].to_string());
+        assert_eq!("3: [P] [C]", &stacks[2].to_string());
+    }
+
+    #[test]
     fn can_pop_from_stack() {
         let mut stack = Stack {
             number: 1,
@@ -141,5 +206,14 @@ mod tests {
         stack.push(&mut to_push);
         assert_eq!('X', stack.crates[0].symbol);
         assert_eq!('A', stack.crates[1].symbol);
+    }
+
+    #[test]
+    fn can_get_top_crate_symbol() {
+        let stack = Stack {
+            number: 1,
+            crates: vec![Crate { symbol: 'A' }, Crate { symbol: 'Z' }],
+        };
+        assert_eq!('Z', stack.top_crate());
     }
 }
--- dec05/src/main.rs
+++ dec05/src/main.rs
@@ -1,3 +1,28 @@
+use dec05::{parse_command, parse_stacks, Command, Crate};
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input1-test.txt").expect("Failed to read input! Wat?1");
+    let parts: Vec<&str> = input.splitn(2, "\n\n").collect();
+
+    // stack
+    let lines_before_empty_line: Vec<&str> = parts[0].lines().collect();
+    let mut stacks = parse_stacks(lines_before_empty_line);
+
+    // commands
+    let lines_after_empty_line: Vec<&str> = parts[1].lines().collect();
+    let commands: Vec<Command> = lines_after_empty_line
+        .iter()
+        .map(|line| parse_command(line))
+        .collect();
+
+    for command in &commands {
+        println!("{}", command);
+        let mut popped = stacks[command.source as usize - 1].pop(command.count);
+        stacks[command.destination as usize - 1].push(&mut popped);
+    }
+
+    let result_char: Vec<char> = stacks.iter().map(|stack| stack.top_crate()).collect();
+    let result_str: String = result_char.into_iter().collect();
+    println!("{}", result_str);
 }
