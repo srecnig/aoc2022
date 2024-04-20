1875d05451d46418d0d3cbc1ed6d5ff5c7f67be6
martin sereinig <ms@veryunited.net>
2024-04-20 10:40:10 +0200

    pt2

--- dec06/src/lib.rs
+++ dec06/src/lib.rs
@@ -1,9 +1,8 @@
 use std::collections::{HashMap, VecDeque};
 
-pub fn detect_marker(signal: &str) -> usize {
-    let offset = 3;
+pub fn detect_marker(signal: &str, marker_size: usize) -> usize {
+    let offset = marker_size - 1;
     let mut window: VecDeque<char> = signal.chars().take(offset).collect();
-
     for (i, ch) in signal.chars().enumerate().skip(offset) {
         window.push_back(ch);
         if !has_duplicate(&window) {
@@ -29,22 +28,24 @@ mod tests {
 
     #[test]
     fn can_find_signal() {
-        let marker = detect_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+        let marker = detect_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4);
         assert_eq!(6, marker);
-        let marker = detect_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
+        let marker = detect_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4);
         assert_eq!(10, marker);
+        let marker = detect_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14);
+        assert_eq!(28, marker);
     }
 
     #[test]
     fn can_handle_string_without_duplicates() {
-        let marker = detect_marker("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        let marker = detect_marker("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 4);
         assert_eq!(usize::MAX, marker);
     }
 
     #[test]
     fn can_check_for_duplicates() {
-        let mut with_duplicate = VecDeque::from(['a', 't', 'm', 'm']);
-        let mut without_duplicate = VecDeque::from(['h', 'a', 's', 'i']);
+        let with_duplicate = VecDeque::from(['a', 't', 'm', 'm']);
+        let without_duplicate = VecDeque::from(['h', 'a', 's', 'i']);
         assert!(has_duplicate(&with_duplicate));
         assert!(!has_duplicate(&without_duplicate));
     }
--- dec06/src/main.rs
+++ dec06/src/main.rs
@@ -3,6 +3,8 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input1.txt").expect("Failed to read input!");
-    let marker = detect_marker(&input);
-    println!("{}", marker + 1);
+    let packet_marker = detect_marker(&input, 4);
+    let message_marker = detect_marker(&input, 14);
+    println!("{}", packet_marker + 1);
+    println!("{}", message_marker + 1);
 }
