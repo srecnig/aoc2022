ceb91540023c1bf6a36b30e8310e4b3af94debe2
martin sereinig <ms@veryunited.net>
2024-04-27 23:20:52 +0200

    left/right is working

--- dec08/src/lib.rs
+++ dec08/src/lib.rs
@@ -41,14 +41,12 @@ impl Forest {
         let mut left_to_right: Vec<&Tree> = vec![];
         for row in &self.trees[1..self.trees.len() - 1] {
             // first and last are always ignored
-            let mut last_tree = &row[0];
+            let mut biggest_height = row[0].height;
             for tree in &row[1..row.len() - 1] {
-                if tree.height > last_tree.height {
+                if tree.height > biggest_height {
                     left_to_right.push(tree);
-                } else {
-                    break;
+                    biggest_height = tree.height;
                 }
-                last_tree = tree;
             }
         }
         println!("********");
@@ -57,18 +55,34 @@ impl Forest {
         let mut right_to_left: Vec<&Tree> = vec![];
         for row in &self.trees[1..self.trees.len() - 1] {
             // first and last are always ignored
-            let mut last_tree: &Tree = &row.last().unwrap();
+            let mut biggest_height = row.last().unwrap().height;
             for tree in row[1..row.len() - 1].iter().rev() {
-                if tree.height > last_tree.height {
+                if tree.height > biggest_height {
                     right_to_left.push(tree);
-                } else {
-                    break;
+                    biggest_height = tree.height;
                 }
-                last_tree = tree;
             }
         }
         println!("#######");
         println!("{:?}", right_to_left);
+
+        // [Tree { x: 2, y: 1, height: 5 }, yes
+        // Tree { x: 3, y: 2, height: 3 },
+        // Tree { x: 1, y: 2, height: 5 }]
+
+        // 30373
+        // 25512
+        // 65332
+        // 33549
+        // 35390
+
+        // The top-left 5 is visible from the left and top. (It isn't visible from the right or bottom since other trees of height 5 are in the way.)
+        // The top-middle 5 is visible from the top and right. yes
+        // The top-right 1 is not visible from any direction; for it to be visible, there would need to only be trees of height 0 between it and an edge.
+        // The left-middle 5 is visible, but only from the right. yes
+        // The center 3 is not visible from any direction; for it to be visible, there would need to be only trees of at most height 2 between it and an edge.
+        // The right-middle 3 is visible from the right. yes
+        // In the bottom row, the middle 5 is visible, but the 3 and 4 are not.
 
         5
     }
